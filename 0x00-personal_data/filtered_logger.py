65075a632d58710f94bb122e2782d7b09cfb52d4
Okoyen E. Precious <x17.ebi@icloud.com>
2024-09-12 23:48:35 +0100

    Added README and project task files

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -29,7 +29,7 @@ def get_logger() -> logging.Logger:
     """
     logger = logging.getLogger("user_data")
     stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))  # Convert tuple to list
     logger.setLevel(logging.INFO)
     logger.propagate = False
     logger.addHandler(stream_handler)
