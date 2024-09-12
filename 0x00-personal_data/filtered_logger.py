fc4f37362cadc73c8764503bf1263b8aee09248a
Okoyen E. Precious <x17.ebi@icloud.com>
2024-09-12 23:49:25 +0100

    Updated code fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -29,7 +29,7 @@ def get_logger() -> logging.Logger:
     """
     logger = logging.getLogger("user_data")
     stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))  # Convert tuple to list
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.setLevel(logging.INFO)
     logger.propagate = False
     logger.addHandler(stream_handler)
